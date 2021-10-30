5d2f8dd5d79dc1bbf1d79ab58682702ad449ac14
Tom Forbes <tom@tomforb.es>
2021-10-30 05:12:07 +0000

    {"name":"loving-wiki","version":"2021.10.30.0","file":"loving_wiki-2021.10.30.0-py3-none-any.whl","path":"loving-wiki/loving_wiki-2021.10.30.0-py3-none-any.whl"}

---
+++ loving-wiki/loving_wiki-2021.10.30.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""loving-wiki
+https://github.com/apachecn/loving-wiki"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2021.10.30.0"
